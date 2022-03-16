31f62c1ce0dc5a8c279e77222b52c5cd5a5ccf97
Megan Bradshaw <mbradshaw4861@conestogac.on.ca>
2022-03-16 17:32:28 -0400

    Added a couple methods for basic routing stubs in front end

--- Application/server/index.js
+++ Application/server/index.js
@@ -13,8 +13,22 @@ app.use(express.static(path.resolve(__dirname, '../eminders/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server (Megan)!" });
-  });
+  res.json({ message: "Hello from the server!" });
+});
+
+// Handle GET requests to /api/stub/login route
+app.get("/api/stub/login", (req, res) => {
+  var list = ["megan", "gabe", "duane"];
+  res.json({ message: list });
+  console.log('Sent name list for get request at /api/stub/login')
+});
+
+// Handle GET requests to /api/stub/main route
+app.get("/api/stub/main", (req, res) => {
+  var list = ["monday", "tuesday", "wednesday"];
+  res.json({ message: list });
+  console.log('Sent day list for get request at /api/stub/main')
+});
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
