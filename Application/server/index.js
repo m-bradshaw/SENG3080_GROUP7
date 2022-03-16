8c3ee16f3fd5897dc8206bdecc0dcde3d9927d90
Gabriel Gurgel <gabsantana23@gmail.com>
2022-03-16 15:56:24 -0400

    Setup backend project structure

--- Application/server/index.js
+++ Application/server/index.js
@@ -1,84 +1,38 @@
-// REFERENCE: https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
-
-// server/index.js
 const path = require('path');
 const express = require("express");
+const reminderRouter = require('./src/routes/reminderRoutes');
 
+// Set express application
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../eminders/build')));
 
+// Set up routes
+app.use('/api/v1/reminder', reminderRouter);
+
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from the server!" });
+  res.json({ message: "Hello from server (Megan)!" });
 });
-
-// Handle GET requests to /api/stub/login route
-app.get("/api/stub/login", (req, res) => {
-  var list = ["megan", "gabe", "duane"];
-  res.json({ message: list });
-  console.log('Sent name list for get request at /api/stub/login')
-});
-
-// Handle GET requests to /api/stub/main route
-app.get("/api/stub/main", (req, res) => {
-
-  var option_1 = {
-    message: "Do a thing", 
-    date: "2001-01-01", 
-    time: "10:00", 
-    recurring: true, 
-    daily: false, 
-    weekly: true, 
-    monthly: false, 
-    yearly: false                
-  }
-  var option_2 = {
-    message: "Do another thing", 
-    date: "2016-08-21", 
-    time: "13:23", 
-    recurring: false, 
-    daily: false, 
-    weekly: false, 
-    monthly: false, 
-    yearly: false                
-  }
-  var option_3 = {
-    message: "Sleep or something?", 
-    date: "2020-03-07", 
-    time: "23:30", 
-    recurring: true, 
-    daily: true, 
-    weekly: false, 
-    monthly: false, 
-    yearly: false                
-  }
-
-  res.json([option_1, option_2, option_3]);
-
-  console.log('Sent day list for get request at /api/stub/main');
-
-});
-
-// Test loading a page with...
-// const birds = require('.birds')
-// app.use('/birds', birds)
-
-// Currently we have the following equivalencies: (assuming both "server" and "eminders" are being served)
-// localhost:5050 == localhost:3000 == localhost:5050/login == localhost:3000/login
-// localhost:3000/Main == localhost:5050/Main
-// localhost:5050/api/stub/login == data for localhost:3000 etc.
-// localhost:5050/api/stub/main == data for localhost:3000/Main etc.
-
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../eminders/build', 'index.html'));
+res.sendFile(path.resolve(__dirname, '../eminders/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5050;
+/* Set up middlewares */
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
   
+  return;
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
---
+++ Application/server/src/routes/reminderRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const reminderCtrl = require('../controllers/remindersController');
+
+// Get all reminders
+router.get('/', reminderCtrl.getMultiple);
+
+// Get one reminder based on their ID
+router.get('/:id', reminderCtrl.get);
+
+// Create a new reminder 
+router.post('/', reminderCtrl.create);
+
+// Update an existing reminder
+router.put('/:id', reminderCtrl.update);
+
+// Delete a specific reminder
+router.delete('/:id', reminderCtrl.remove);
+
+module.exports = router;
