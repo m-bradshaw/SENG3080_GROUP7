4061795884da495992ebb741bfa013881c559591
Megan Bradshaw <mbradshaw4861@conestogac.on.ca>
2022-03-16 23:27:20 -0400

    Added basic validation to message form

--- Application/eminders/src/components/Main.js
+++ Application/eminders/src/components/Main.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import GetData from './HandleData';
 import { NavLink} from 'react-router-dom';
+import '../css/layout.css';
+import MessageForm from './MessageForm';
 
-import {Button, Container, Form, Row, Col} from 'react-bootstrap';
+import {Button, Container, Row, Col} from 'react-bootstrap';
 
 class Main extends Component {
 
@@ -85,75 +87,7 @@ class Main extends Component {
           </Row>
 
           <Row>
-
-            <Form>
-              <Row>
-                <Col></Col>
-                  <Col xs={8}>
-                    <h4>
-                      Create/Edit Reminder:
-                    </h4> 
-                    <Form.Group className="mb-3" controlId='formTextMessage'>
-                      <Form.Label>Message:</Form.Label>
-                      <Form.Control as="textarea" rows={5} placeholder='Enter message here...'></Form.Control>      
-                    </Form.Group>        
-                    <Form.Group className="mb-3" controlId='formDate'>
-                      <Form.Label>Date:</Form.Label>
-                      <Form.Control type="date" placeholder='Enter message here...'></Form.Control>      
-                    </Form.Group>        
-                    <Form.Group className="mb-3" controlId='formTime'>
-                      <Form.Label>Time:</Form.Label>
-                      <Form.Control type="time" placeholder='Enter message here...'></Form.Control>      
-                    </Form.Group> 
-                  </Col>
-                <Col></Col>
-              </Row>
-
-              <Row>
-                <Col></Col>
-                <Col xs={8}>
-                  <Form.Group className="mb-3" controlId="formRecurringCheckbox">
-                    <Form.Check type="checkbox" label="Recurring" />
-                  </Form.Group>  
-                  <Form.Group className="mb-3" controlId="formTimeRadios">
-                    <Form.Check inline type="radio" name="timeGroup" label="Daily" />
-                    <Form.Check inline type="radio" name="timeGroup" label="Weekly" />
-                    <Form.Check inline type="radio" name="timeGroup" label="Monthly" />
-                    <Form.Check inline type="radio" name="timeGroup" label="Yearly" />
-                  </Form.Group> 
-                </Col>
-                <Col></Col>                 
-              </Row>
-              
-              <Row>
-                <Col></Col>     
-                <Col xs={8}>
-                  <Form.Group className="mb-3" controlId="formDayCheckbox">
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Sunday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Monday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Tuesday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Wednesday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Thursday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Friday" />
-                    <Form.Check inline type="checkbox" name="dayGroup" label="Saturday" />
-                  </Form.Group>
-                </Col>     
-                <Col></Col>     
-              </Row>
-              
-              <Row className='alignCenter'>
-                <Col></Col>
-                <Col>
-                  <Button variant='primary' type='submit'>Submit</Button>                  
-                </Col>
-                <Col>
-                  <Button variant='teritary' type='cancel'>Cancel</Button>
-                </Col>                
-                <Col></Col>                
-              </Row>
-
-            </Form> 
-
+            <MessageForm></MessageForm>
           </Row>
 
           <Row>
---
+++ Application/eminders/src/components/MessageForm.js
@@ -0,0 +1,86 @@
+import {Button, Form, Row, Col} from 'react-bootstrap';
+import '../css/layout.css';
+import {React, useState} from 'react';
+
+function MessageForm(props) {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    return (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row>
+            <Col></Col>
+                <Col xs={8}>
+                <h4>
+                    Create/Edit Reminder:
+                </h4> 
+                <Form.Group className="mb-3" controlId='formTextMessage'>
+                    <Form.Label>Message:</Form.Label>
+                    <Form.Control as="textarea" rows={5} placeholder='Enter message here...' required></Form.Control>      
+                </Form.Group>        
+                <Form.Group className="mb-3" controlId='formDate'>
+                    <Form.Label>Date:</Form.Label>
+                    <Form.Control type="date" required></Form.Control>      
+                </Form.Group>        
+                <Form.Group className="mb-3" controlId='formTime'>
+                    <Form.Label>Time:</Form.Label>
+                    <Form.Control type="time" required></Form.Control>      
+                </Form.Group> 
+                </Col>
+            <Col></Col>
+            </Row>
+
+            <Row>
+            <Col></Col>
+            <Col xs={8}>
+                <Form.Group className="mb-3" controlId="formRecurringCheckbox">
+                <Form.Check type="checkbox" label="Recurring" />
+                </Form.Group>  
+                <Form.Group className="mb-3" controlId="formTimeRadios">
+                <Form.Check inline type="radio" name="timeGroup" label="Daily" />
+                <Form.Check inline type="radio" name="timeGroup" label="Weekly" />
+                <Form.Check inline type="radio" name="timeGroup" label="Monthly" />
+                <Form.Check inline type="radio" name="timeGroup" label="Yearly" />
+                </Form.Group> 
+            </Col>
+            <Col></Col>                 
+            </Row>
+            
+            <Row>
+            <Col></Col>     
+            <Col xs={8}>
+                <Form.Group className="mb-3" controlId="formDayCheckbox">
+                <Form.Check inline type="checkbox" name="dayGroup" label="Sunday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Monday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Tuesday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Wednesday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Thursday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Friday" />
+                <Form.Check inline type="checkbox" name="dayGroup" label="Saturday" />
+                </Form.Group>
+            </Col>     
+            <Col></Col>     
+            </Row>
+            
+            <Row className='alignCenter'>
+            <Col></Col>
+            <Col>
+                <Button variant='primary' type='submit'>Submit</Button>                  
+            </Col>             
+            <Col></Col>                
+            </Row>
+
+        </Form> 
+    );
+}
+
+export default MessageForm; 
