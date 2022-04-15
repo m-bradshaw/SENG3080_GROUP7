72cde9eee658b516db327ff52c90a1126ae17411
Gabriel Gurgel <gabsantana23@gmail.com>
2022-04-14 20:54:39 -0400

    Add middleware to express app

--- Application/server/index.js
+++ Application/server/index.js
@@ -1,18 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require("cors");
 
-require("dotenv").config()
-const PORT = process.env.PORT || 3001;
-
 const mongoose = require('mongoose');
-// middleware = require
+const middleware = require('./src/middlewares')
 const api = require('./src/routes');
 
 // Set express application
 const app = express();
-
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
@@ -23,20 +20,11 @@ mongoose.connect(process.env.DB_SOURCE, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('open', () => console.log('Connected to MongoDB'));
 
-// Set up routes
 app.use('/api/v1', api);
-
-/* Set up middlewares */
-// Error handling middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-  
-  return;
-});
+app.use(middleware.notFound);
 
 // Start the server
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
