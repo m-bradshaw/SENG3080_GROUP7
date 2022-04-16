9000cc5a49a474faa30b661a8ece54745ebb0ff5
Megan Bradshaw <mbradshaw4861@conestogac.on.ca>
2022-04-15 22:07:04 -0400

    Deleted message form, moved contents to main

--- Application/eminders/src/components/Main.js
+++ Application/eminders/src/components/Main.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import RequestJsonData from './HandleData';
 import { NavLink} from 'react-router-dom';
 import '../css/layout.css';
-import MessageForm from './MessageForm';
-import {Button, Container, Row, Col, Stack, ListGroup} from 'react-bootstrap';
+import {Button, Container, Row, Col, Stack, ListGroup, Form} from 'react-bootstrap';
 import ExistingMessage from './ExistingMessage';
 
 class Main extends Component {
@@ -13,17 +12,28 @@ class Main extends Component {
     super(props); 
 
     this.state = {
-        dataList: [],
-        currentData: null, // We want to be able to set this with data from a clicked reminder (to edit)
+      dataList: [],
+      currentData: null
     }
     this.dataSource = "api/stub/main";
 
     // Ensure component state is bound to changes from the following methods
-    this.setData = this.setData.bind(this); 
-    this.messageFormSubmit = this.messageFormSubmit.bind(this);     
-    this.messageFormReset = this.messageFormReset.bind(this);     
+    this.setExistingRemindersList = this.setExistingRemindersList.bind(this); 
+    this.handleFormReset = this.handleFormReset.bind(this);     
     this.existingMessageDelete = this.existingMessageDelete.bind(this); 
     this.existingMessageEdit = this.existingMessageEdit.bind(this); 
+    this.handleFormSubmit = this.handleFormSubmit.bind(this); 
+  }
+  
+  // Fetch the list on first mount
+  componentDidMount() {
+    console.log("Main - Mount");
+    RequestJsonData(this.dataSource, this.setExistingRemindersList);
+  }
+
+  componentDidUpdate() {
+    console.log("Main - Update");
+    // RequestJsonData(this.dataSource, this.setData); 
   }
 
   logState() {
@@ -37,55 +47,53 @@ class Main extends Component {
     alert(message); 
   }
 
-  // Fetch the list on first mount
-  componentDidMount() {
-    console.log("Main - Mount");
-    RequestJsonData(this.dataSource, this.setData);
+
+
+  logOutButton = () => {
+    return (
+      <Container className='m-3'>
+        <NavLink to={'../login'}>
+          <Button>Log Out</Button>
+        </NavLink>  
+      </Container>
+    )
   }
 
-  componentDidUpdate() {
-    console.log("Main - Update");
-    // RequestJsonData(this.dataSource, this.setData); 
-  }
+
 
   // Sets the list of existing reminders
-  setData = (json) => {
+  setExistingRemindersList = (json) => {
     if (json) {
         this.setState({dataList: json});
     }
     this.logState(); 
   }
 
-  // This method can be called from the MessageForm
-  // Basically in here we want to be able to push the data to the server for either post or patch
-  messageFormSubmit = (json) => {
-    if (json) {
-        console.log("messageFormSubmit called in Main");
-        console.log(json); 
-        alert(json); 
+  // Map the incoming server data to a list of ExistingMessage components
+  mapExistingMessages = (show) => {
+    const jsonObject = this.state.dataList;
+    if (show) {
+        return (
+            <Container className="alignCenter">
+              <ListGroup variant="flush">
+                {
+                  jsonObject.map((value, index) => {
+                      return (
+                        <ListGroup.Item key={index}>
+                          <ExistingMessage values={value} key={index} editClickHandler={this.existingMessageEdit} deleteClickHandler={this.existingMessageDelete}>{index}</ExistingMessage>                                                    
+                        </ListGroup.Item>                            
+                      );
+                  })
+                }
+              </ListGroup>
+            </Container>
+        );
     }
-    this.setState({currentData: null});
-
-    //this.logState(); 
-  }
-
-  messageFormReset = (json) => {
-
-    this.triggerAlert("Message form Reset triggered!");
-    if (json) {
-        console.log("messageFormReset called in Main");
+    else {
+        return (
+            <div>NO ITEMS AVAILABLE FROM SERVER</div>
+        );
     }
-    this.setState({currentData: null});
-
-    //this.logState(); 
-  }
-
-  // Button handler for editing an existing message. 
-  // In here we want to be able to fill in the fields of the create/edit area (and reload it) with the values from the existing message. 
-  existingMessageEdit = (data) => {
-    console.log("Existing message edit clicked");
-    this.setState({currentData: data.values});
-    console.log(data); 
   }
 
   // We should probably do a toast popup or something to confirm in here before actually removing from our list. 
@@ -96,44 +104,127 @@ class Main extends Component {
     this.triggerAlert(msg); 
   }
 
-  render() {
-    console.log("Rendering Main:");
-    const jsonObject = this.state.dataList;
-    //this.logState();
+  // Button handler for editing an existing message. 
+  // In here we want to be able to fill in the fields of the create/edit area (and reload it) with the values from the existing message. 
+  existingMessageEdit = (data) => {
+    this.setState({
+      currentData: data.values
+    });
+  }
 
-    // Map the incoming server data to a list of ExistingMessage components
-    var mapData = (show) => {
-        if (show) {
-            return (
-                <Container className="alignCenter">
-                  <ListGroup variant="flush">
-                    {
-                      jsonObject.map((value, index) => {
-                          return (
-                            <ListGroup.Item key={index}>
-                              <ExistingMessage values={value} key={index} editClickHandler={this.existingMessageEdit} deleteClickHandler={this.existingMessageDelete}>{index}</ExistingMessage>                                                    
-                            </ListGroup.Item>                            
-                          );
-                      })
-                    }
-                  </ListGroup>
-                </Container>
-            );
+
+  
+  mainForm = () => {
+    return (
+      <Form noValidate validated={this.state.validated} onSubmit={this.handleFormSubmit} onReset={this.handleFormReset}>
+
+        <Form.Group className="mb-3" controlId='formTextMessage'>
+            <Form.Label>Message:</Form.Label>
+            <Form.Control as="textarea" rows={5} required placeholder='Enter message here...' defaultValue={(this.props.fillData) ? this.props.fillData.message : ""}></Form.Control>      
+        </Form.Group>        
+
+        <Form.Group className="mb-3" controlId='formDate'>
+            <Form.Label>Date:</Form.Label>
+            <Form.Control type="date" required defaultValue={(this.props.fillData) ? this.props.fillData.date : null}></Form.Control>           
+        </Form.Group>       
+
+        <Form.Group className="mb-3" controlId='formTime'> 
+            <Form.Label>Time:</Form.Label>
+            <Form.Control type="time" required defaultValue={(this.props.fillData) ? this.props.fillData.time : null}></Form.Control>      
+        </Form.Group>
+
+        <Stack direction="horizontal" gap={3} className="alignCenter">
+
+            <Form.Group className="mb-3" controlId="formRecurringCheckbox" >
+                <Form.Check type="checkbox" label="Recurring" defaultChecked={(this.props.fillData) ? this.props.fillData.recurring : false}/>
+            </Form.Group>  
+
+            <Form.Group className="mb-3" controlId="formTimeRadios">
+                <Form.Check inline type="radio" name="timeGroup" label="Daily" id="Daily" checked={
+                    this.state.daily
+                } onChange={this.handleRadioChange}/>
+                <Form.Check inline type="radio" name="timeGroup" label="Weekly" id="Weekly" checked={
+                    this.state.weekly
+                } onChange={this.handleRadioChange}/>
+                <Form.Check inline type="radio" name="timeGroup" label="Monthly" id="Monthly" checked={
+                    this.state.monthly
+                } onChange={this.handleRadioChange}/>
+                <Form.Check inline type="radio" name="timeGroup" label="Yearly" id="Yearly" checked={
+                    this.state.yearly
+                } onChange={this.handleRadioChange}/>
+            </Form.Group> 
+    
+        </Stack>
+
+        <Stack direction="horizontal" gap={3} className='alignCenter'>
+            <Button variant='primary' type='submit'>Submit</Button>  
+            <Button variant='primary' type='reset' value="Reset">Reset</Button>  
+        </Stack>
+
+      </Form> 
+    )
+  }
+
+  handleFormSubmit = (event) => {
+
+    console.log(this.props); 
+    console.log(this.state); 
+    console.log(event); 
+
+    const form = event.currentTarget;
+    const valid = form.checkValidity(); 
+    if (!valid) {
+        // Stop the page from reloading
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    else {
+      console.log("Handling Valid Submission");
+
+      if (event.target) {
+        var newData = {
+            message: event.target[0].value, 
+            date: event.target[1].value, 
+            time: event.target[2].value, 
+            recurring: event.target[3].checked, 
+            daily: event.target[4].checked, 
+            weekly: event.target[5].checked, 
+            monthly: event.target[6].checked, 
+            yearly: event.target[7].checked                
         }
-        else {
-            return (
-                <div>NO ITEMS AVAILABLE FROM SERVER</div>
-            );
-        }
+  
+        // Call the response handler to set the data in the main component
+        console.log(newData); 
+        alert(newData); 
+      }  
     }
 
+    // Validation has been done (set colours)
+    this.setState({
+        validated: true
+    });
+  };
+
+  handleFormReset = (json) => {
+
+    this.triggerAlert("Message form Reset triggered!");
+    if (json) {
+        console.log("messageFormReset called in Main");
+    }
+    this.setState({
+      currentData: null
+    });
+    
+  }
+
+
+
+  render() {
+    console.log("Rendering Main:");
     return (
       <div>
-        <Container className='m-3'>
-          <NavLink to={'../login'}>
-            <Button>Log Out</Button>
-          </NavLink>  
-        </Container>
+
+        {this.logOutButton()}
 
         <Container className='flex'>
           <div className='m-2'>
@@ -142,13 +233,13 @@ class Main extends Component {
               <h1 className='alignCenter'>Welcome!</h1>
 
               <h4>Create/Edit Reminder:</h4> 
-              <MessageForm submitResponseHandler={this.messageFormSubmit} resetResponseHandler={this.messageFormReset} fillData={this.state.currentData}></MessageForm>
+              
+              {this.mainForm()}
 
               <h4>My Reminders:</h4> 
 
               <div>
-                { mapData(this.state.dataList.length >= 1) }    
-                {/* THIS IS WHERE THE LIST OF REMINDERS SHOULD GO */}
+                { this.mapExistingMessages(this.state.dataList.length >= 1) }    
               </div>
 
             </Stack>
